00d374afa0f4ed84ad28a5f70e54656678f75ab7
RomuloJB <jordaobogoni@gmail.com>
2025-06-03 22:36:02 -0300

    add | Repositorio Fuel implementado

--- src/main/java/com/onixx/apolloveiculos/api/Repositories/FuelRepository.java
+++ src/main/java/com/onixx/apolloveiculos/api/Repositories/FuelRepository.java
@@ -1,4 +1,23 @@
 package com.onixx.apolloveiculos.api.Repositories;
 
-public interface FuelRepository {
+import com.onixx.apolloveiculos.api.Domains.Fuel.Fuel;
+import com.onixx.apolloveiculos.api.Domains.Standard.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FuelRepository extends JpaRepository<Fuel, Integer> {
+
+    @Query("SELECT f FROM Fuel f WHERE f.id_fuel = :id")
+    Fuel findByIdFuel(@Param("id") Long id);
+
+    @Query("SELECT f FROM Fuel f WHERE " +
+            "(:name IS NULL OR f.name = :name) AND " +
+            "(:status IS NULL OR f.status = :status)")
+    List<Fuel> findByFilters(@Param("name") String name, @Param("status") Status status);
 }
