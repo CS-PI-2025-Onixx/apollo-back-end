82ba64382c9520985786e73c758bb74b8ac3dc2f
RomuloJB <jordaobogoni@gmail.com>
2025-06-27 20:37:31 -0300

    update | Repositorio da transmissao implementado

--- src/main/java/com/onixx/apolloveiculos/api/Repositories/TransmissionsRepository.java
+++ src/main/java/com/onixx/apolloveiculos/api/Repositories/TransmissionsRepository.java
@@ -1,6 +1,6 @@
 package com.onixx.apolloveiculos.api.Repositories;
 
-import com.onixx.apolloveiculos.api.Domains.Motors.Motors;
+import com.onixx.apolloveiculos.api.Domains.Transmissions.Transmissions;
 import com.onixx.apolloveiculos.api.Domains.Standard.Status;
 import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,14 +12,14 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface MotorsRepository extends JpaRepository<Motors, Integer> {
-    Motors findByName(String name);
+public interface TransmissionsRepository extends JpaRepository<Transmissions, Integer> {
+    Transmissions findByName(String name);
 
-    @Query("SELECT m FROM Motors m WHERE m.id_motor = :id")
-    Motors findByIdMotors(@Param("id") Long id);
+    @Query("SELECT m FROM Transmissions m WHERE m.id_transmission = :id")
+    Transmissions findByIdTransmissions(@Param("id") Long id);
 
-    @Query("SELECT m FROM Motors m WHERE " +
+    @Query("SELECT m FROM Transmissions m WHERE " +
             "(:name IS NULL OR m.name = :name) AND " +
             "(:status IS NULL OR m.status = :status)")
-    List<Motors> findByFilters(@Param("name") String name, @Param("status") Status status);
+    List<Transmissions> findByFilters(@Param("name") String name, @Param("status") Status status);
 }
