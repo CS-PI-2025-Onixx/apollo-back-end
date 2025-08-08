9deb7b8cad4e582a121f2af5c4d482f4462b8d1a
RomuloJB <jordaobogoni@gmail.com>
2025-08-08 20:26:45 -0300

    add | dto directions implementado

--- src/main/java/com/onixx/apolloveiculos/api/Domains/Directions/Directions.java
+++ src/main/java/com/onixx/apolloveiculos/api/Domains/Directions/Directions.java
@@ -1,4 +1,4 @@
-package com.onixx.apolloveiculos.api.Domains.Direction;
+package com.onixx.apolloveiculos.api.Domains.Directions;
 
 import com.onixx.apolloveiculos.api.Domains.Standard.Standard;
 import jakarta.persistence.*;
@@ -15,7 +15,7 @@ import org.hibernate.annotations.Where;
 @Data
 @SQLDelete(sql="UPDATE tb_direction SET dt_delete = CURRENT_TIMESTAMP WHERE id_direction = ?")
 @Where(clause = "dt_delete is NULL")
-public class Direction extends Standard {
+public class Directions extends Standard {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_direction;
