f16cbedd80044bbc0db4e091c814a9de0cb09ff8
RomuloJB <jordaobogoni@gmail.com>
2025-08-26 22:10:59 -0300

    add | delete em userService implementado

--- src/main/java/com/onixx/apolloveiculos/api/Services/UserService.java
+++ src/main/java/com/onixx/apolloveiculos/api/Services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.onixx.apolloveiculos.api.Domains.User.User;
+import com.onixx.apolloveiculos.api.Domains.User.UserDTO;
 import com.onixx.apolloveiculos.api.Repositories.UserRepository;
 
 import jakarta.transaction.Transactional;
@@ -41,5 +42,17 @@ public class UserService implements UserDetailsService {
         return userRepository.findByName(username);
     }
 
+        @Transactional
+    public User delete(UserDTO userDTO) {
+        User user = (User) userRepository.findByName(userDTO.name());
+        if (user == null) {
+            throw new IllegalArgumentException("Usuário não encontrado: " + userDTO.name());
+        }
+        if (!passwordEncoder.matches(userDTO.password(), user.getPassword())) {
+            throw new IllegalArgumentException("Senha incorreta");
+        }
+        userRepository.deleteById(user.getId_user());
+        return user;
+    }
     
 }
