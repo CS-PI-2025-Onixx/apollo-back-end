71e1a4184cec2c35710c119b286f4b06bc1094dd
Claudir Fantuci <claudirfantucijunior@gmail.com>
2025-09-19 20:35:12 -0300

    add | inicio Refresh token

--- src/main/java/com/onixx/apolloveiculos/api/Infra/Security/TokenService.java
+++ src/main/java/com/onixx/apolloveiculos/api/Infra/Security/TokenService.java
@@ -34,6 +34,19 @@ public class TokenService {
             throw new RuntimeException("Erro na geração do JWT", e);
         }
     }
+    public String generateRefreshToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API")
+                    .withSubject(user.getName())
+                    .withClaim("type", "refresh")
+                    .withExpiresAt(genRefreshTokenExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro na geração do refresh token", e);
+        }
+    }
 
     public String validateToken(String token) {
         try {
@@ -49,11 +62,15 @@ public class TokenService {
     }
 
     private Instant genExpirationDate() {
-        return LocalDateTime.now().plusHours(30).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(24).toInstant(ZoneOffset.of("-03:00"));
     }
 
     private Instant genExpirationDateForReset() {
-        return LocalDateTime.now().plusHours(10).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusMinutes(10).toInstant(ZoneOffset.of("-03:00"));
+    }
+    
+    private Instant genRefreshTokenExpirationDate() {
+        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-03:00"));
     }
 
     public String generateResetPasswordToken(String email) {
