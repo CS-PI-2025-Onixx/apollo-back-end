2e78e64dc7d7858c0ceb2fcf9e8db0008a6ae9b1
RomuloJB <jordaobogoni@gmail.com>
2025-10-14 22:26:43 -0300

    add | entidade FavoriteCars criada

---
+++ src/main/java/com/onixx/apolloveiculos/api/Domains/Cars/FavoriteCars.java
@@ -0,0 +1,46 @@
+package com.onixx.apolloveiculos.api.Domains.Cars;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import com.onixx.apolloveiculos.api.Domains.User.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_favorite_cars")
+@SQLDelete(sql = "UPDATE tb_favorite_cars SET dt_delete = CURRENT_TIMESTAMP WHERE id_car = ?")
+@Where(clause = "dt_delete IS NULL")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FavoriteCars {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_favorite_car")
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_user", nullable = false)
+    private User user;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_car", nullable = false)
+    private Cars car;
+
+    @Column(name = "dt_created", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
